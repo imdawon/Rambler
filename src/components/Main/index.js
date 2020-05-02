cdac5d40e65dfdb7362b420f36f6ac595f66b083
Noisey Turtle <bobbyoldani2@gmail.com>
2020-05-02 10:58:36 -0700

    installed react router dom

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "../Navbar";
 import Search from "../../pages/Search";
 import Completed from "../../pages/Completed";
