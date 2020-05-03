25b3446f2821f4288587009a4d85f7d11b88a9e3
dhens <dawsonhensel@gmail.com>
2020-05-03 13:57:04 -0700

    removed routes require as we are using React Router. added mongoose to package.json

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const routes = require("./routes");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,8 +11,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist", {
