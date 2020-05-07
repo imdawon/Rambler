b61421432c7913d6f7b9790e72dbf63b22f96aac
Dawson Hensel <dawsonhensel@gmail.com>
2020-05-07 15:32:02 -0700

    now all auth code is conatined in Auth page. we are now using react-google-login npm module to authenticate now

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,11 +1,22 @@
 import React from "react";
+import GoogleLogin from "react-google-login"
 
 function Auth() {
-  
+
+  const responseGoogle = (response) => {
+    console.log(response)
+  }
+
   return (
     <div>
       <h1>Weclome to Rambler Sign-in</h1>
-      <div className="g-signin2" data-onsuccess="onSignIn"></div>
+      <GoogleLogin
+        clientId="174389928584-m3a4ljtpsks71r09srq96fobsujg8l9v.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
     </div>
   );
 }
