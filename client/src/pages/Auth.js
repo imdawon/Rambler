eb7352aa68a9f786fa7d77767e86678262f59cd2
Dawson Hensel <dawsonhensel@gmail.com>
2020-05-07 14:17:04 -0700

    removed homebrew code for google oauth in Auth.js and replaced with google sign in boilerplate

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -3,26 +3,14 @@ import * as queryString from 'query-string';
 
 function Auth() {
 
-    const stringifiedParams = queryString.stringify({
-        client_id: process.env.REACT_APP_OAUTH_CLIENT_ID,
-        redirect_uri: 'https://shielded-reaches-07010.herokuapp.com/google-auth/',
-        scope: [
-          'https://www.googleapis.com/auth/userinfo.email',
-          'https://www.googleapis.com/auth/userinfo.profile',
-        ].join(' '), // space seperated string
-        response_type: 'code',
-        access_type: 'offline',
-        prompt: 'consent',
-      });
-      
-      const googleLoginUrl = `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`;
-      console.log(`googleLoginUrl: ${googleLoginUrl}`);
-      console.log(`CLIENT ID: ${process.env.OAUTH_CLIENT_ID}`);
+  const onSignIn = (googleUser) => {
+    console.log(`User is ${googleUser.getBasicProfile}`)
+  }
 
   return (
     <div>
-        <h1>Weclome to Rambler Sign-in</h1>
-        <a href={googleLoginUrl}>Sign up & in With Google</a>
+      <h1>Weclome to Rambler Sign-in</h1>
+      <div className="g-signin2" data-onsuccess="onSignIn"></div>
     </div>
   );
 }
