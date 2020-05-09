e5ee26bf76bba10375a21c31c56626c1f79fad87
Dawson Hensel <dawsonhensel@gmail.com>
2020-05-08 17:41:41 -0700

    removed react-google-login from Auth.js and all related code within

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,29 +1,10 @@
 import React from "react";
-import GoogleLogin from "react-google-login";
-// import axios from 'axios';
 
 function Auth() {
-
-  const responseGoogle = (response) => {
-    // id we need to pass to our backend
-    const id_token = response.tc.id_token;
-    // axios.post(`/google-auth`, id_token)
-    // .then(res => {
-    //   console.log(res);
-    // })
-  }
-
   return (
     <div>
       <h1>Weclome to Rambler Sign-in</h1>
       <a href="/google-auth">Login With Google</a>
-      <GoogleLogin
-        clientId={process.env.OAUTH_CLIENT_ID}
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-      />
     </div>
   );
 }
