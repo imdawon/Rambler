9bae223a892ecabb0c2df3ca31e11129186a652c
Noisey Turtle <bobbyoldani2@gmail.com>
2020-05-17 11:30:18 -0700

    outline for data visualization complete.

--- client/src/pages/Log.js
+++ client/src/pages/Log.js
@@ -1,8 +1,14 @@
 import React from "react";
+import LineChart from "../components/LineChart";
+import BarChart from "../components/BarChart";
 
 function Log() {
     return (
+        <div>
         <p>Log</p>
+        <LineChart />
+        <BarChart />
+        </div>
     );
 }
 
