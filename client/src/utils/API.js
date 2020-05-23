b544371bfaa2605d8c0f4a46bb8408f49d86c2ed
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-05-23 11:58:57 -0700

    added max results min of 12

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 export default {
+    //location iq for lat/lon
     getLocation: function(searchLocation) {
         return axios.get("https://us1.locationiq.com/v1/search.php?key=" + process.env.REACT_APP_LOCATION_API_KEY + "&q=" + searchLocation + "&format=json")
     },
-    getTrails: function(lat, lon, maxDistance) {
-        return axios.get("https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + lon + "&maxDistance=" + maxDistance + "&key=" + process.env.REACT_APP_REI_API_KEY)
+    // rei api for hike data
+    getTrails: function(lat, lon, maxDistance, maxResults) {
+        return axios.get("https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + lon + "&maxDistance=" + maxDistance + "&maxResults=" + maxResults + "&key=" + process.env.REACT_APP_REI_API_KEY)
     },
+    // openweather for hike detail weather forecast
     getWeather: function(lat, lon) {
         return axios.get("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=metric&appid=" + process.env.REACT_APP_OPENWEATHER_API_KEY)
     },
@@ -25,7 +28,6 @@ export default {
         return axios.put("/api/user/log/remove/" + id, hikeToRemove);
     },
     getDetails: function(url) {
-        
         return axios.get(url);
     }
 };
