db98b74dc6d6466ddc9df2b89a178cc58e0a0b2d
James Dobbe <dobbe2@gmail.com>
2020-05-27 13:37:46 -0700

    weather style

--- client/src/components/Weather/index.js
+++ client/src/components/Weather/index.js
@@ -41,24 +41,26 @@ function Weather() {
         <div className='weather'>
             <div className="main_weather">
                 <div className='weather_title'>
-                    <p className='weather_title'>Conditions</p>
-                    <p className='weather_city'>{state.forecastLocation}</p>
+                    <h1 className='weather_title'>Local Weather Conditions</h1>
+                    <h1 className='weather_city'>{state.forecastLocation}</h1>
                 </div>
             </div>
-            <div className='weather_cards_div'>
+            <div className='cards'>
                 {state.weather.map((day, i) => (
-                    <div className="weather_card" key={i}>
-                        <p>Date:
+                    <div id="display" className="cards_item" key={i}>
+                        <div className="card">
+                            <div className="card_content is-centered no_white">
+                                <img className="weather_icon" src={`https://openweathermap.org/img/w/${day.weather[0].icon}.png`} alt="wthr img" />
+
+                                <p className="card_text">Date:
                 <Moment format="MM/DD h:mm">{day.dt_txt}</Moment>
-                        </p>
-                        <div className='weather_img'>
-                            <img src={`https://openweathermap.org/img/w/${day.weather[0].icon}.png`} alt="wthr img" />
-                        </div>
-                        <p>Temp: {(Math.floor((day.main.temp * 9 / 5) + 32))} °F</p>
-                        <p>Humidity: {day.main.humidity} %</p>
-                        <p>Wind: {day.wind.speed} MPH</p>
-                        <p>Expect {day.weather[0].description}</p>
-                    </div>
+                                </p>
+
+                                <p className="card_text red_color">Temp: {(Math.floor((day.main.temp * 9 / 5) + 32))} °F</p>
+                                <p className="card_text">Humidity: {day.main.humidity} %</p>
+                                <p className="card_text">Wind: {day.wind.speed} MPH</p>
+                                <p className="card_text">Expect {day.weather[0].description}</p>
+                            </div></div></div>
                 ))}
             </div>
         </div>
