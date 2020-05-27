32684a2418372c1fc3936894a8984d25165ab122
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-05-26 21:47:16 -0700

    details button fixes, pagination completed

--- client/src/components/ButtonDetails/index.js
+++ client/src/components/ButtonDetails/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function ButtonDetails() {
+function ButtonDetails(props) {
     
     return (
-        <button className="btn card_btn">Details</button>
+        <button className="btn card_btn"{...props}>Details</button>
     );
 };
 
