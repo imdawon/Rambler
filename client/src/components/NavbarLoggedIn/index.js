7ed2bda18cb84600960beaa489f21cc24acb35e0
Dawson Hensel <dawsonhensel@gmail.com>
2020-06-03 21:03:14 -0700

    reverted hamburger

--- client/src/components/NavbarLoggedIn/index.js
+++ client/src/components/NavbarLoggedIn/index.js
@@ -47,7 +47,7 @@ function NavbarLoggedIn() {
     </a>
   </div>
 
-  <div id="navbarBasicExample" className="navbar-menu">
+  <div id="navbarBasicExample" className="navbar-menu is-active">
     <div className="navbar-start">
       <div className="navbar-item">
         <div className="buttons">
